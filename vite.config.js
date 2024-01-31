273e1712b79070b83a343f0af6372eecb022a6bd
Om Prakash <om@techxillence.com>
2024-01-31 14:44:11 +0545

    chnages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 //https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/vite-deploy/"
+  base: "/"
+  // build: {
+  //   // other build options
+  //   rollupOptions: {
+  //     input: 'src/main.jsx',
+  //   },
+  // },
 })
