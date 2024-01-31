fc23c5dd0a653494178076b68598dc48af48c8fd
Om Prakash <om@techxillence.com>
2024-01-31 07:57:26 +0545

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,50 @@
+// import content
+import { useEffect } from "react";
+import { content } from "../Content";
+const Hero = () => {
+  const { hero } = content;
+
+  return (
+    <section id="home" className="overflow-hidden">
+      <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
+
+        {/* first col */}
+        <div className="" style={{height:'70vh'}} data-aos="fade-down">
+        <h6> Hello i'm </h6>
+            <h2>{hero.firstName}{' '}{hero.LastName}</h2>
+          <h6>{hero.title}</h6>
+          <br />
+          <div className="flex justify-end">
+            <button className="btn">{hero.btnText}</button>
+          </div>
+          <div className="flex flex-col gap-10 mt-10">
+            {hero.hero_content.map((content, i) => (
+              <div
+                key={i}
+                data-aos="fade-down"
+                data-aos-delay={i * 300}
+                className={`flex items-center w-80 gap-5
+            ${i === 1 && " flex-row-reverse text-right"}  `}
+              >
+                <h3>{content.count}</h3>
+                <p>{content.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* sec col */}
+        <div className="md:h-[47rem] h-40">
+          <img
+            src={hero.image}
+            data-aos="slide-up"
+            alt="..."
+            className="h-full object-cover"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
