805e975c605a5ea1a5c024dea038b39ee0bb6d6a
Om Prakash <om@techxillence.com>
2024-02-01 09:15:13 +0545

    update

--- src/Content.js
+++ src/Content.js
@@ -12,7 +12,7 @@ import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
 import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/Projects/crm.png";
+import project1 from "./assets/images/Projects/img1.png";
 import project2 from "./assets/images/Projects/img2.png";
 import project3 from "./assets/images/Projects/img3.png";
 import person_project from "./assets/images/Projects/person.png";
